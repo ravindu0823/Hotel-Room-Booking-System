333c49793f3232014ca44d615fe0100a81401d0a
ravindu0823 <guestpc87@gmail.com>
2023-12-18 03:22:24 +0530

    fix: Remove unwanted navs in sidenav.jsx

--- Client/Admin/src/widgets/layout/sidenav.jsx
+++ Client/Admin/src/widgets/layout/sidenav.jsx
@@ -25,42 +25,41 @@ export function Sidenav({ brandImg, brandName, routes }) {
       } fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl border border-blue-gray-100 transition-transform duration-300 xl:translate-x-0`}
     >
       <div className="m-4">
-        {routes.map(({ layout, title, pages }, key) => (
-          <ul key={key} className="mb-4 flex flex-col gap-1">
-            {pages.map(({ icon, name, path }) =>
-              name === "sign in" ? (
-                <div></div>
-              ) : (
-                <li key={name}>
-                  <NavLink to={`/${layout}${path}`}>
-                    {({ isActive }) => (
-                      <Button
-                        variant={isActive ? "gradient" : "text"}
-                        color={
-                          isActive
-                            ? sidenavColor
-                            : sidenavType === "dark"
-                            ? "white"
-                            : "blue-gray"
-                        }
-                        className="flex items-center gap-4 px-4 capitalize"
-                        fullWidth
-                      >
-                        {icon}
-                        <Typography
-                          color="inherit"
-                          className="font-medium capitalize"
+        {routes.map(
+          ({ layout, title, pages }, key) =>
+            layout === "dashboard" && (
+              <ul key={key} className="mb-4 flex flex-col gap-1">
+                {pages.map(({ icon, name, path }) => (
+                  <li key={name}>
+                    <NavLink to={`/${layout}${path}`}>
+                      {({ isActive }) => (
+                        <Button
+                          variant={isActive ? "gradient" : "text"}
+                          color={
+                            isActive
+                              ? sidenavColor
+                              : sidenavType === "dark"
+                              ? "white"
+                              : "blue-gray"
+                          }
+                          className="flex items-center gap-4 px-4 capitalize"
+                          fullWidth
                         >
-                          {name}
-                        </Typography>
-                      </Button>
-                    )}
-                  </NavLink>
-                </li>
-              ),
-            )}
-          </ul>
-        ))}
+                          {icon}
+                          <Typography
+                            color="inherit"
+                            className="font-medium capitalize"
+                          >
+                            {name}
+                          </Typography>
+                        </Button>
+                      )}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ),
+        )}
       </div>
     </aside>
   );
