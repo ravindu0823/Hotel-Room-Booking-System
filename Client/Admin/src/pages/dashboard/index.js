54f7ac30bba54989c2c4bb3ee7ff7caa85d48cc7
ravindu0823 <guestpc87@gmail.com>
2023-12-18 03:16:40 +0530

    Added exports

--- Client/Admin/src/pages/dashboard/index.js
+++ Client/Admin/src/pages/dashboard/index.js
@@ -2,3 +2,6 @@ export * from "@/pages/dashboard/Home";
 export * from "@/pages/dashboard/Profile";
 export * from "@/pages/dashboard/Tables";
 export * from "@/pages/dashboard/notifications";
+export * from "@/pages/dashboard/Reservations";
+export * from "@/pages/dashboard/AddReservation"
+export * from "@/pages/dashboard/UpdateReservation"
