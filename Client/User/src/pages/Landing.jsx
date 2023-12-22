381db489041b6e014d7936fa08f599c65fd43dfb
ravindu0823 <guestpc87@gmail.com>
2023-12-23 02:48:47 +0530

    chore: added useEffect hook and commented :)

--- Client/User/src/pages/Landing.jsx
+++ Client/User/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Comrades from "../components/Comrades.jsx";
 import Contact from "../components/Contact.jsx";
 import Footer from "../components/Footer.jsx";
@@ -6,9 +6,39 @@ import Hero from "../components/Hero.jsx";
 import Knowledge from "../components/Knowledge.jsx";
 import Midheading from "../components/Midheading.jsx";
 import Navbar from "../components/Navbar.jsx";
-import Toil from "../components/Toil.jsx";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios, { USER_PROTECTED_URL } from "../api/axios.js";
 
 const Landing = () => {
+  /* const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (!token) {
+      navigate("/sign-in");
+      return;
+    }
+
+    const validateToken = async () => {
+      try {
+        const res = await axios.get(USER_PROTECTED_URL, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!res.statusText) throw new Error("Not Authorized");
+      } catch (error) {
+        console.error(error);
+        navigate("/sign-in");
+      }
+    };
+
+    validateToken();
+  }, [navigate]); */
+  
   return (
     <div>
       <Navbar />
