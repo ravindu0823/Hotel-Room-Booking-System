0d0fed0f0a79c2dbd26dd244dbac5209d4867e5a
Sakya Rasadi <sakyarasadi@gmail.com>
2023-12-27 15:24:07 +0530

    Add login route to axios.js

--- Client/User/src/api/axios.js
+++ Client/User/src/api/axios.js
@@ -4,6 +4,7 @@ export const USER_PROTECTED_URL = "/users/protected";
 export const USER_REGISTER_URL = "/users/register";
 export const GET_USER_BY_ID_URL = "/users";
 export const CREATE_RESERVATION_URL = "/reservations/create-existing-user";
+export const loginRoute = "/users/login";
 
 export default axios.create({
   baseURL: "http://localhost:3000",
