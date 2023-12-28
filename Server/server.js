c1f645ba161168845ff8fd4961f47061c1e49270
chamithra1999 <117897659+chamithra1999@users.noreply.github.com>
2023-12-28 14:01:58 +0530

    updated server.js in server side

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,34 @@
-import { connectToDB } from "./db/conn.js";
-const PORT = process.env.PORT || 3000;
-import app from "./app.js";
 
-// start the Express server
-app.listen(PORT, async () => {
-  await connectToDB();
-  console.log(`Server is listening on port ${PORT}`);
+import { connectToDB } from "./db/conn.js";
+import express from "express";
+import bodyParser from "body-parser";
+import app from "./app.js";
+ 
+const PORT = process.env.PORT || 3000;
+ 
+// Middleware setup
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+ 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
+ 
+// Start the Express server
+app.listen(PORT, async () => {
+  try {
+    await connectToDB();
+    console.log(`Server is listening on port ${PORT}`);
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+});
+ 
