754b7a349bd06899e2b40ca9311a2b5e44f25ce5
ravindu0823 <guestpc87@gmail.com>
2023-12-31 16:16:13 +0530

    chore: Add background images to tailwind.config.js

--- Client/User/tailwind.config.js
+++ Client/User/tailwind.config.js
@@ -10,6 +10,12 @@ export default withMt({
         orbitron: ["Orbitron", "sans-serif"],
         oswald: ["Oswald", "sans-serif"],
       },
+      backgroundImage: {
+        "sign-in-page":
+          "url('https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        "reservation-page":
+          "url('https://cloudbeds-fcfc.kxcdn.com/wp-content/uploads/2022/05/iStock-1068158510-scaled-e1669046834676.jpg')",
+      },
     },
   },
   plugins: [import("@tailwindcss/forms")],
