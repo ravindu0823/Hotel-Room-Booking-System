5c815dd94f32ae5254ff0a2de5d2b4a4610111c4
ravindu0823 <guestpc87@gmail.com>
2023-12-31 16:28:24 +0530

    Add ChatUser model
    Co-authored-by: Sakya Rasadi <sakyarasadi@gmail.com>

---
+++ Server/models/chatUser.js
@@ -0,0 +1,36 @@
+import pkg from "mongoose";
+const { Schema, model, models } = pkg;
+import bcrypt from "bcrypt";
+
+const ChatUserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: "",
+  },
+});
+
+const ChatUser = models.ChatUser || model("ChatUser", ChatUserSchema);
+
+export default ChatUser;
