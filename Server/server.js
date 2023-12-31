c85613c5fe68a2bed068a1f919e6e62dd6e4a09a
ravindu0823 <guestpc87@gmail.com>
2023-12-31 16:28:12 +0530

    Add socket.io integration for real-time messaging
    Co-authored-by: Sakya Rasadi <sakyarasadi@gmail.com>

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,43 @@
 import { connectToDB } from "./db/conn.js";
 import app from "./app.js";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 3000;
 
 // Start the Express server
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   try {
     await connectToDB();
     console.log(`Server is listening on port ${PORT}`);
   } catch (error) {
     console.error("Failed to connect to the database:", error);
   }
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5000",
+      "http://localhost:8000",
+    ],
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
