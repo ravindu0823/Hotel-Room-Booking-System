d7ed53567e46da683824771c560771c5610b5565
chamithra1999 <117897659+chamithra1999@users.noreply.github.com>
2024-01-02 02:07:28 +0530

    Added endpoint

--- Client/User/src/pages/RoomDetails.jsx
+++ Client/User/src/pages/RoomDetails.jsx
@@ -15,6 +15,29 @@ const RoomDetails = () => {
   const { id } = useParams();
   const [room, setRoom] = useState(null);
 
+  useEffect(() => {
+    const fetchRoomData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/rooms/${id}`);
+        setRoom(response.data);
+
+        // Print room data to the console
+        console.log("Room Data:", response.data);
+      } catch (error) {
+        console.error("Error fetching room data:", error);
+      }
+    };
+
+    fetchRoomData();
+  }, [id]);
+  // Simulating four smaller images
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };``
+
   // Your room data in Database
   
 
