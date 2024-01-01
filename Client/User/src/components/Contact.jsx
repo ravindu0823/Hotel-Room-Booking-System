e48e634ea9c6b55b704a74951235d27ec07bb03d
chamithra1999 <117897659+chamithra1999@users.noreply.github.com>
2024-01-02 03:12:51 +0530

    modify contact us component

--- Client/User/src/components/Contact.jsx
+++ Client/User/src/components/Contact.jsx
@@ -9,27 +9,27 @@ const Contact = () => {
           We&apos;re just one email away.&nbsp;Hit us up!
         </h3>
         <div>
-          <form className="space-y-2 flex flex-col text-justify items-center justify-around p-4 mt-4 mb-auto border-[2px] border-black rounded-2xl bg-blue-100 max-w-6xl w-[380px] lg:w-[900px] xl:w-[1200px] sm:w-[600px] md:w-[680px] h-[500px]">
+          <form className="space-y-2 flex flex-col text-justify items-center justify-around p-4 mt-4 mb-auto border-[2px] border-black rounded-2xl bg-black max-w-5xl w-[380px] lg:w-[900px] xl:w-[1200px] sm:w-[600px] md:w-[680px] h-[500px]">
             <input
-              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-[beige] placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
+              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="text"
               placeholder="NAME"
               required
             />
             <input
-              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-[beige] placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
+              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="email"
               placeholder="EMAIL"
               required
             />
             <textarea
-              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-[beige] placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
+              className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="text"
               placeholder="MESSAGE"
               rows="6"
               required
             />
-            <button className="p-3 m-2 rounded-2xl border-[2px] border-slate-900 hover:bg-[#111] hover:text-white ease-in duration-300">
+            <button className="p-3 m-2 rounded-2xl border-[2px] border-slate-900 hover:bg-white hover:text-black bg-white ease-in duration-300">
               SUBMIT
             </button>
           </form>
