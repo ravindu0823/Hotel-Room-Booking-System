5c400c37f986f9cef2c62be33fb77e629d369b8b
chamithra1999 <117897659+chamithra1999@users.noreply.github.com>
2024-01-02 02:13:59 +0530

    Add new card

--- Client/User/src/pages/RoomDetails.jsx
+++ Client/User/src/pages/RoomDetails.jsx
@@ -51,6 +51,9 @@ const RoomDetails = () => {
        'url("https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
    }}
  > 
+ <div className="flex relative z-20 items-center overflow-hidden">
+  
+ </div>
  </div> 
  </>
   );
