b5dd708a9c53f24b862914baba055e37d8f731f7
chamithra1999 <117897659+chamithra1999@users.noreply.github.com>
2024-01-02 02:12:23 +0530

    Added Background image

--- Client/User/src/pages/RoomDetails.jsx
+++ Client/User/src/pages/RoomDetails.jsx
@@ -36,14 +36,23 @@ const RoomDetails = () => {
 
   const handleImageClick = (image) => {
     setSelectedImage(image);
-  };``
+  };
 
   // Your room data in Database
   
 
 
   return (
-   <></>
+   <>
+   <div
+   className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
+   style={{
+     backgroundImage:
+       'url("https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+   }}
+ > 
+ </div> 
+ </>
   );
 };
 
