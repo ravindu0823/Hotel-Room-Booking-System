e701f0c79d4289b4d0ab1c2cb6c8131ad9b3e721
ravindu0823 <guestpc87@gmail.com>
2024-01-02 21:28:13 +0530

    feat: Refactor PayPal integration in Payment component

--- Client/User/src/pages/Payment.jsx
+++ Client/User/src/pages/Payment.jsx
@@ -1,31 +1,92 @@
-import { useState } from "react";
-import PayPal from "../components/PayPal";
-import { Button } from "@material-tailwind/react";
+import { CLIENT_ID } from "../utils/PaypalConfig";
+import { useEffect, useState } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const Payment = () => {
-  const [checkout, setCheckout] = useState(false);
+  const [show, setShow] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState("");
+  const [orderID, setOrderID] = useState(false);
+
+  // creates a paypal order
+  const createOrder = (data, actions) => {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            description: "Sunflower",
+            amount: {
+              currency_code: "USD",
+              value: 20,
+            },
+          },
+        ],
+      })
+      .then((orderID) => {
+        setOrderID(orderID);
+        return orderID;
+      });
+  };
+
+  // check Approval
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      const { payer } = details;
+      setSuccess(true);
+    });
+  };
+
+  //capture likely error
+  const onError = (data, actions) => {
+    setErrorMessage("An Error occured with your payment ");
+  };
+
+  useEffect(() => {
+    if (success) {
+      alert("Payment successful!!");
+      console.log("Order successful . Your order id is--", orderID);
+    }
+  }, [success]);
 
   return (
-    <>
-      {checkout ? (
-        <PayPal />
-      ) : (
-        <div>
-          <Button
-            color="blue"
-            buttonType="filled"
-            size="regular"
-            rounded={false}
-            block={false}
-            iconOnly={false}
-            ripple="light"
-            onClick={() => setCheckout(true)}
-          >
-            Checkout
-          </Button>
+    <PayPalScriptProvider options={{ "client-id": CLIENT_ID }}>
+      <div>
+        <div className="wrapper">
+          <div className="product-img">
+            <img
+              src="https://cdn.pixabay.com/photo/2021/08/15/06/54/sunflower-6546993_1280.jpg"
+              alt="SunFlower"
+              height="320"
+              width="300"
+            />
+          </div>
+          <div className="product-info">
+            <div className="product-text">
+              <h1>Sunflower</h1>
+            </div>
+            <div className="product-price-btn">
+              <p>$20</p>
+              <br></br>
+              <button
+                className="buy-btn"
+                type="submit"
+                onClick={() => setShow(true)}
+              >
+                Buy now
+              </button>
+            </div>
+          </div>
         </div>
-      )}
-    </>
+        <br></br>
+        {show ? (
+          <PayPalButtons
+            style={{ layout: "vertical" }}
+            createOrder={createOrder}
+            onApprove={onApprove}
+          />
+        ) : null}
+      </div>
+    </PayPalScriptProvider>
   );
 };
 
