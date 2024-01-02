f2c21928431071d574b4cdd2e0e77a538fcc1253
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 02:32:25 +0530

    Add empty div in Userprofile component

--- Client/User/src/pages/Userprofile.jsx
+++ Client/User/src/pages/Userprofile.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 function Userprofile() {
   return (
     <div class="flex flex-col lg:flex-row bg-white">
+        <div class=" lg:w-1/4">
+            
+        </div>
     </div>
   )
 }
