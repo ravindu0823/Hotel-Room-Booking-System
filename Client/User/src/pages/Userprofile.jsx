19d960cb99bb9885e2e1ae336dbc47b3ee571b5b
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 02:35:35 +0530

    Add user profile image

--- Client/User/src/pages/Userprofile.jsx
+++ Client/User/src/pages/Userprofile.jsx
@@ -18,6 +18,13 @@ function Userprofile() {
                 alt="Mountain"
               />
             </div>
+            <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
+              <img
+                className="object-cover object-center h-32"
+                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+                alt="Woman looking front"
+              />
+            </div>
           </div>
         </div>
       </div>
