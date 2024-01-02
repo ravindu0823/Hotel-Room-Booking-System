f9265e1d04c91ca7ab976ddc6d327dc7eb04c621
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 02:33:17 +0530

    Add back to home button in Userprofile.jsx

--- Client/User/src/pages/Userprofile.jsx
+++ Client/User/src/pages/Userprofile.jsx
@@ -4,7 +4,11 @@ function Userprofile() {
   return (
     <div class="flex flex-col lg:flex-row bg-white">
         <div class=" lg:w-1/4">
-            
+        <div class="p-4 border-t mx-8">
+          {/* <button class="w-3/4 block mx-auto rounded-full bg-transparent hover:bg-gray-200 hover:text-gray-900 hover:shadow-lg font-semibold text-gray-900 px-6 py-2 border border-gray-900">
+            Back to Home
+          </button> */}
+        </div>
         </div>
     </div>
   )
