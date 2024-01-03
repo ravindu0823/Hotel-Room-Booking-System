92236c76d0d562ec0dba95ffd1154c0a28512df9
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-04 01:21:13 +0530

    Add "Read more" link to FoodCard component

--- Client/User/src/components/FoodCard.jsx
+++ Client/User/src/components/FoodCard.jsx
@@ -151,6 +151,29 @@ const FoodCard = ({ food }) => {
                 +20
               </span>
             </Tooltip>
+            <div className="text-left">
+            <Link
+                to={`/foods/${food._id}`}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-dark dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </Link>
+            </div>
         </CardBody>
         Hello foods
       </Card>
