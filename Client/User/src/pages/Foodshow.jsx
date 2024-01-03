a62b73b5beae2355f7ff6f2d1ad3b9a4ed2a081d
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 23:37:11 +0530

    Add Contact and Footer components to Foodshow page

--- Client/User/src/pages/Foodshow.jsx
+++ Client/User/src/pages/Foodshow.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Midheading from '../components/Midheading'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Midheading from "../components/Midheading";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 const Foodshow = () => {
   return (
@@ -9,9 +11,11 @@ const Foodshow = () => {
       <Navbar />
       <Hero />
       <Midheading />
-
+      {/* <Foodshow /> */}
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Foodshow
+export default Foodshow;
