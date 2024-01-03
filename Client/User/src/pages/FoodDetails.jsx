ee8ba10a022a6d622d1efab7d02ea730b0fd6f53
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-04 01:28:57 +0530

    Add useState, useEffect, and other dependencies to FoodDetails component

--- Client/User/src/pages/FoodDetails.jsx
+++ Client/User/src/pages/FoodDetails.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+} from "@material-tailwind/react";
 
 function FoodDetails() {
   return (
