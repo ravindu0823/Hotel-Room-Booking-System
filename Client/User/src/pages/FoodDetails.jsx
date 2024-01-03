7b81a3c8f0b0a014018d42e387a938f35e6eca05
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-04 01:31:17 +0530

    Add food state and background images to FoodDetails component

--- Client/User/src/pages/FoodDetails.jsx
+++ Client/User/src/pages/FoodDetails.jsx
@@ -13,6 +13,14 @@ import {
 } from "@material-tailwind/react";
 
 function FoodDetails() {
+  const [food, setFoods] = useState(null);
+  const { id } = useParams();
+  const [backgroundImages, setBackgroundImages] = useState([
+    "https://img.freepik.com/premium-vector/restaurant-mural-wallpaper_23-2148685950.jpg?w=1380", // Add your image URLs here
+    "https://img.freepik.com/free-vector/restaurant-mural-wallpaper_23-2148703851.jpg?w=1060&t=st=1704154021~exp=1704154621~hmac=d12dd462625f7f7bc5ca199e9cb49cab3db9d7e2156bd0bcfef6d0d8401857f6",
+    "https://wallpaperaccess.com/full/6169319.jpg",
+  ]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   return (
     <div>FoodDetails</div>
   )
