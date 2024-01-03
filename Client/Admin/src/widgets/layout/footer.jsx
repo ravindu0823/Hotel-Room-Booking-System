f846ace9ef3fd50ad5c0ee4d597d04819988c7e2
ravindu0823 <guestpc87@gmail.com>
2024-01-03 20:45:31 +0530

    refactor: Update footer text and styling

--- Client/Admin/src/widgets/layout/footer.jsx
+++ Client/Admin/src/widgets/layout/footer.jsx
@@ -6,12 +6,12 @@ export function Footer({ brandName, brandLink, routes }) {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="py-2">
+    <footer className="py-2 flex justify-end">
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
         <Typography variant="small" className="font-normal text-inherit">
           &copy; {year}, made with{" "}
           <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" />{" "}
-          by Group-25 @ NSBM university for a Fullstack development.
+          by Group-25 @ NSBM Green University for a Fullstack Development Project.
         </Typography>
       </div>
     </footer>
