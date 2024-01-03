8657f782ba0e6af2dff77db476ac8c7808a8d704
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 23:27:36 +0530

    Add Midheading component to Foodshow page

--- Client/User/src/pages/Foodshow.jsx
+++ Client/User/src/pages/Foodshow.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Midheading from '../components/Midheading'
 
 const Foodshow = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Midheading />
+
     </div>
   )
 }
