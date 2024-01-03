14736b102fcfd6b917533f60dfe2cd9a1007de6a
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-04 00:53:41 +0530

    Refactor Foods component

--- Client/User/src/pages/Foods.jsx
+++ Client/User/src/pages/Foods.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect } from "react";
-import FoodCard from "../components/FoodCard";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import FoodCard from '../components/FoodCard'; 
+import axios from 'axios';
 
 const Foods = () => {
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
+    // Fetch food data from the backend
     const fetchFoods = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/foods/read"); // Replace with your actual API endpoint for food
+        const response = await axios.get('http://localhost:3000/foods/read'); // Replace with your actual API endpoint for food
         setFoods(response.data);
       } catch (error) {
-        console.error("Error fetching food data:", error);
+        console.error('Error fetching food data:', error);
       }
     };
+
     fetchFoods();
-  }, []);
+  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+
   return (
     <div>
-        <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center">
         {foods.map((food) => (
           <FoodCard key={food._id} food={food} />
-        ))} 
-        </div>
+        ))}
+      </div>
     </div>
   );
 };
