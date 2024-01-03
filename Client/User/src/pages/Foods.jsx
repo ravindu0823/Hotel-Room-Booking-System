07a23c100d38590ef76aff189402e0af2a9e27d6
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 23:58:09 +0530

    Refactor Foods component to use useEffect and axios for fetching food data

--- Client/User/src/pages/Foods.jsx
+++ Client/User/src/pages/Foods.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import FoodCard from '../components/FoodCard';
-import axios from 'axios';
-
+import React, { useEffect } from "react";
+import FoodCard from "../components/FoodCard";
+import axios from "axios";
 
 const Foods = () => {
-    const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState([]);
 
-    
+  useEffect(() => {
+    const fetchFoods = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/foods/read"); // Replace with your actual API endpoint for food
+        setFoods(response.data);
+      } catch (error) {
+        console.error("Error fetching food data:", error);
+      }
+    };
+    fetchFoods();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center">
+        {foods.map((food) => (
+          <FoodCard key={food._id} food={food} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-}
-
-// function Foods() {
-//   return (
-//     <div>Foods</div>
-//   )
-// }
-
-export default Foods
+export default Foods;
