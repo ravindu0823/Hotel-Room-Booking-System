621c820ca2cf4fb24b063c7381d1e62d2c8e5c6c
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-04 00:37:56 +0530

    Add axios, Link, and Material-UI imports to FoodCard component

--- Client/User/src/components/FoodCard.jsx
+++ Client/User/src/components/FoodCard.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
-
+import axios, { GET_ALL_FOOD_URL } from "../api/axios";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Tooltip,
+  Button,
+} from "@material-tailwind/react";
 function FoodCard() {
   return (
     <div>FoodCard</div>
