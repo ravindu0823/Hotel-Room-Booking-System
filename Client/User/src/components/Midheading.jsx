6dd570e9e58b7f96c55e17492f7efec61d61bf33
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 23:19:12 +0530

    Update Midheading component to use Link for room facility

--- Client/User/src/components/Midheading.jsx
+++ Client/User/src/components/Midheading.jsx
@@ -15,13 +15,9 @@ const Midheading = () => {
       {/* <p className="text-lg font-medium hover:underline underline-offset-4 decoration-2 decoration-[dimgray]">
         Reservation
       </p> */}
-      <Link
-        to="/rooms"
-        className="text-lg font-medium hover:underline underline-offset-4 decoration-2 decoration-[dimgray]"
-        onClick={toggleRoom} // Click handler to toggle visibility
-      >
-        Rooms
-      </Link>
+      <p className="text-lg font-medium hover:underline underline-offset-4 decoration-2 decoration-[dimgray]">
+      <Link to="/rooms">Our Room Facilty</Link>
+      </p>
       <p className="text-lg font-medium hover:underline underline-offset-4 decoration-2 decoration-[dimgray]">
       <Link to="/foodshow">Our Food Items</Link>
       </p>
