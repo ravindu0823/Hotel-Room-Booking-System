ef0ef9770d7a0bfac0c213dac6987ef41bf4445c
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2024-01-03 23:26:48 +0530

    Add Navbar and Hero components to Foodshow page

--- Client/User/src/pages/Foodshow.jsx
+++ Client/User/src/pages/Foodshow.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 const Foodshow = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Hero />
     </div>
   )
 }
