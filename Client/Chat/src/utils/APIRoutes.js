fc3370c8416a4f1fbf233e142103f00017a2df62
ravindu0823 <guestpc87@gmail.com>
2024-01-05 02:08:28 +0530

    refactor: Update APIRoutes.js to use dynamic host based on environment

--- Client/Chat/src/utils/APIRoutes.js
+++ Client/Chat/src/utils/APIRoutes.js
@@ -1,4 +1,7 @@
-export const host = "http://localhost:3000";
+export const host =
+  process.env.NODE_ENV === "production"
+    ? "http://159.223.74.216:3000"
+    : "http://localhost:3000";
 export const loginRoute = `${host}/chat/login`;
 export const registerRoute = `${host}/chat/register`;
 export const logoutRoute = `${host}/chat/logout`;
