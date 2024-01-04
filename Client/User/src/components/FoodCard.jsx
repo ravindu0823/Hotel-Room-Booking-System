ad766d72e18e701c6f2e5f21cf8f76639eea2030
ravindu0823 <guestpc87@gmail.com>
2024-01-05 02:11:10 +0530

    refactor: Refactor import statement in FoodCard component

--- Client/User/src/components/FoodCard.jsx
+++ Client/User/src/components/FoodCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios, { GET_ALL_FOOD_URL } from "../api/axios";
 import { Link } from "react-router-dom";
 import {
@@ -7,7 +7,6 @@ import {
   CardBody,
   Typography,
   Tooltip,
-  Button,
 } from "@material-tailwind/react";
 
 const FoodCard = ({ food }) => {
