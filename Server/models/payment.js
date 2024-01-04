0dadf39d999a3d8d6bb82e61b670addd77362134
ravindu0823 <guestpc87@gmail.com>
2024-01-04 19:02:42 +0530

    feat: Add Payment model

---
+++ Server/models/payment.js
@@ -0,0 +1,30 @@
+import pkg from "mongoose";
+const { Schema, model, models, Types } = pkg;
+
+const PaymentSchema = new Schema({
+  transactionId: {
+    type: String,
+    required: [true, "Please add a transactionId"],
+  },
+
+  reservationId: {
+    type: Types.ObjectId,
+    ref: "Reservation",
+    required: [true, "Please add reservationId"],
+  },
+
+  userId: {
+    type: Types.ObjectId,
+    ref: "User",
+    required: [true, "Please add userId"],
+  },
+
+  amount: {
+    type: Number,
+    required: [true, "Please add amount"],
+  },
+});
+
+const Payment = models.Payment || model("Payment", PaymentSchema);
+
+export default Payment;
