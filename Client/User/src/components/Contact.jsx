06a7080aca230f4111367cc01b3cb6c0b828ef27
ravindu0823 <guestpc87@gmail.com>
2024-01-06 03:10:44 +0530

    feat: Add feedback submission functionality to Contact component

--- Client/User/src/components/Contact.jsx
+++ Client/User/src/components/Contact.jsx
@@ -1,4 +1,46 @@
+import { useState } from "react";
+import { FeedbackModel } from "../utils/models";
+import axios, { SEND_FEEDBACK } from "../api/axios";
+import Swal from "sweetalert2";
+import { Navigate } from "react-router-dom";
+
 const Contact = () => {
+  const [feedback, setFeedback] = useState(FeedbackModel);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        SEND_FEEDBACK,
+        JSON.stringify(feedback),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+
+      console.log(response);
+
+      if (!response.statusText) throw new Error("Feedback Submission Failed");
+
+      Swal.fire({
+        title: "Hotel Room Booking System",
+        text: `Thank you for your feedback! an email has been sent to ${feedback.email}`,
+        icon: "success",
+      }).then(() => {
+        Navigate("/");
+      });
+    } catch (err) {
+      console.log(err);
+      Swal.fire({
+        title: "Hotel Room Booking System",
+        text: `Feedback Submission Failed!`,
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <div id="seek" className="w-full min-h-screen relative">
       <div className="max-w-[1400px] h-[680px] flex flex-col justify-center items-center space-y-2 p-4 mt-2 mb-auto mx-auto">
@@ -13,39 +55,42 @@ const Contact = () => {
             <input
               className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="text"
+              value={feedback.name}
+              onChange={(e) =>
+                setFeedback({ ...feedback, name: e.target.value })
+              }
               placeholder="NAME"
               required
             />
             <input
               className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="email"
+              value={feedback.email}
+              onChange={(e) =>
+                setFeedback({ ...feedback, email: e.target.value })
+              }
               placeholder="EMAIL"
               required
             />
             <textarea
               className="w-full p-2 m-2 rounded-xl border-slate-700 border bg-white placeholder-gray-800/50 font-oswald tracking-wide leading-snug text-stone-00 uppercase text-lg"
               type="text"
+              value={feedback.feedback}
+              onChange={(e) =>
+                setFeedback({ ...feedback, feedback: e.target.value })
+              }
               placeholder="MESSAGE"
               rows="6"
               required
             />
-            <button className="p-3 m-2 rounded-2xl border-[2px] border-slate-900 hover:bg-white hover:text-black bg-white ease-in duration-300">
+            <button
+              className="p-3 m-2 rounded-2xl border-[2px] border-slate-900 hover:bg-white hover:text-black bg-white ease-in duration-300"
+              onClick={handleSubmit}
+            >
               SUBMIT
             </button>
           </form>
         </div>
-        {/* <div className="flex lg:flex-row justify-around items-center sm:flex-col text-justify p-2 space-x-6 space-y-4">
-          <h3 className="self-start text-xl text-slate-800 tracking-normal leading-snug drop-shadow-2xl rounded-lg shadow-black shadow-md p-2 bg-[beige]/70">
-            Lorem 123, ipsum dolor, sit amet consectetur adipisicing.
-          </h3>
-          <p className="self-center text-lg font-inter text-slate-800 tracking-normal leading-snug drop-shadow-2xl rounded-lg shadow-black shadow-md p-2 bg-[beige]/70">
-            Lorem ipsum dolor sit amet.
-          </p>
-          <p className="self-end text-lg font-inter text-slate-800 tracking-normal leading-snug drop-shadow-2xl rounded-lg shadow-black shadow-md p-2 bg-[beige]/70">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-            praesentium!
-          </p>
-        </div> */}
       </div>
     </div>
   );
