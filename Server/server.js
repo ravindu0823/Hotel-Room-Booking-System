4131b2421e662ef5dd32b3fab3537bffc11c9fda
ravindu0823 <guestpc87@gmail.com>
2024-01-10 01:59:06 +0530

    Add HTTP and HTTPS server support

--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,60 @@
 import { connectToDB } from "./db/conn.js";
 import app from "./app.js";
 import { Server } from "socket.io";
+import fs from "fs";
+import https from "https";
+import http from "http";
+const HTTP_PORT = process.env.PORT || 3000;
+const HTTPS_PORT = process.env.PORT || 8443;
 
-const PORT = process.env.PORT || 3000;
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
 
 // Start the Express server
-const server = app.listen(PORT, async () => {
+/* const server = app.listen(PORT, async () => {
   try {
     await connectToDB();
     console.log(`Server is listening on port ${PORT}`);
   } catch (error) {
     console.error("Failed to connect to the database:", error);
   }
+}); */
+
+const server = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+
+server.listen(HTTP_PORT, async () => {
+  try {
+    await connectToDB();
+    console.log(`Server is listening on port ${HTTP_PORT}`);
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
 });
 
-const io = new Server(server, {
+httpsServer.listen(HTTPS_PORT, async () => {
+  try {
+    await connectToDB();
+    console.log(`Https Server is listening on port ${HTTPS_PORT}`);
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+});
+
+const io = new Server(httpsServer, {
   cors: {
     origin: [
+      "http://localhost:5000",
       "http://frontend-admin",
       "http://frontend-user",
       "http://frontend-chat",
       "http://frontend-user:5000",
       "http://frontend-admin:5173",
       "http://frontend-chat:8000",
+      "https://cinnamonred.companiaa.online",
+      "https://cinnamonred.companiaa.online:5000",
     ],
     credentials: true,
   },
